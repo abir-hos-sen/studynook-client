cd20080723c5c1d6b9735d6570d99d0481decb71
abirkhan0495 <abirkhan0495@gmail.com>
2026-05-17 20:57:05 +0600

    Setup application routing

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,77 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Rooms from "../pages/Rooms";
+import RoomDetails from "../pages/RoomDetails";
+import AddRoom from "../pages/AddRoom";
+import MyListings from "../pages/MyListings";
+import UpdateRoom from "../pages/UpdateRoom";
+import MyBookings from "../pages/MyBookings";
+import NotFound from "../pages/NotFound";
+import PrivateRoute from "./PrivateRoute";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/rooms",
+                element: <Rooms />,
+            },
+            {
+                path: "/rooms/:id",
+                element: <RoomDetails />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/add-room",
+                element: (
+                    <PrivateRoute>
+                        <AddRoom />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/my-listings",
+                element: (
+                    <PrivateRoute>
+                        <MyListings />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/update-room/:id",
+                element: (
+                    <PrivateRoute>
+                        <UpdateRoom />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/my-bookings",
+                element: (
+                    <PrivateRoute>
+                        <MyBookings />
+                    </PrivateRoute>
+                ),
+            }
+        ]
+    }
+]);
+
+export default router;
